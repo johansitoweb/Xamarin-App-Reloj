62f150f4ad24b8b9e71f6a919965034efdf0cd89
johan antonio <mancebojohan97@gmail.com>
2024-12-25 18:13:16 -0400

    johansitoweb

---
+++ WatchShop/WatchShop/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.SharedTransitions;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WatchShop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new[] { "Shapes_Experimental" });
+            MainPage = new SharedTransitionNavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
